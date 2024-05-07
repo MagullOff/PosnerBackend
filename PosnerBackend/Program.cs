f0d19b53a3de0263149b3ac5611af666449feb44
magulloff <przemczar2001@gmail.com>
2024-05-07 22:53:36 +0200

    add swagger

--- PosnerBackend/Program.cs
+++ PosnerBackend/Program.cs
@@ -14,6 +14,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddTransient<GameResultRepository>();
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseCors(corsPolicyBuilder =>
     corsPolicyBuilder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
 );
